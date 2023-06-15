e338f78757b6e9d9fa7528f43751a5857bdd13e1
P-Scholar23 <136748839+P-Scholar23@users.noreply.github.com>
2023-06-15 14:40:22 -0500

    Add files via upload

---
+++ data_list = [5,2,9,1,14,7].py
@@ -0,0 +1,12 @@
+data_list = [5,2,9,1,14,7]
+empty_list = []
+
+#access data in list
+#each item has index position, the 1 here refers to index spot 2 (START AT 0)
+print (data_list[1])
+print (data_list [5])
+
+#loop through list
+length_of_list = len(data_list)
+for index in range(length_of_list):
+    print(data_list[index])
